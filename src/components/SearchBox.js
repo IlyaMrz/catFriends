5ecd33ddb76f92c702ca3556457ce3ecd7a5cbfe
morozov ilya <knpkaa@gmail.com>
2021-02-01 16:00:19 +0300

    style imrpv

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({ searchChange }) => {
     return (
         <div id='top'>
             <h1 className="pa0 ma1">Cat Friends~!</h1>
-            <input className='pa2 ba b--green bg-lightest-blue '
+            <input className='pa2 ba b--green bg-lightest-blue tc'
                 type='search'
                 placeholder='search cats'
                 onChange={searchChange}
