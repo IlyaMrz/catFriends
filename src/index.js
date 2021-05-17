ecf6fc381278aa7241e11508b46e1fcaa4be4019
morozov ilya <knpkaa@gmail.com>
2021-05-17 12:32:35 +0300

    pwa

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './index.css';
 import App from './containers/App3.js';
 import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 // import App from './App'; 
 // import reportWebVitals from './reportWebVitals';
@@ -31,3 +32,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+serviceWorkerRegistration.register();
